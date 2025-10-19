719e6b20923fc07cc2fddbacd5b9f471ea9b6dea
Md Hasibul Islam <polok.hasibul@gmail.com>
2025-10-19 02:05:48 +0200

    2nd Version

--- utils/configExtended.py
+++ utils/configExtended.py
@@ -5,16 +5,16 @@ MASK_DIR = "Masks_c1_c2_660"
 
 MODEL_PATH = "saved_models/unet_bestExtended.pth"
 
-EPOCHS = 120                # small batch ধীরে শেখে → একটু বেশি epochs
+EPOCHS = 120                
 BATCH_SIZE = 4
-LEARNING_RATE = 1e-4        # = 2e-4 * (4/8)
-PATIENCE = 20               # ধীরে improve হবে → patience বাড়াও
+LEARNING_RATE = 1e-4        
+PATIENCE = 20               
 RANDOM_SEED = 42
 USE_FP16 = True
 OPTIMIZER = "AdamW"
 WEIGHT_DECAY = 1e-2
 SCHEDULER = "CosineAnnealingLR"
-WARMUP_EPOCHS = 5           # ছোট batch এ warmup বেশি সহায়ক
+WARMUP_EPOCHS = 5           
 GRADIENT_CHECKPOINTING = True
 GRAD_ACCUM_STEPS = 1
 
